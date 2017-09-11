79ff46aedb5d8d3fb2888772070fa5dfa0bd69a3
Coffeeteer <tgrieshiemer@gmail.com>
2017-09-10 22:24:54 -0400

    now dropping a collection

--- test/test_helper.js
+++ test/test_helper.js
@@ -7,3 +7,10 @@ mongoose.connect('mongodb://localhost/test3', {useMongoClient: true});
 mongoose.connection
   .on('error', console.error.bind(console, 'Error in connection with Mongoose DB'))
   .once('open', () => console.log('You have successfully connected to Mongoose'));
+
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    done();
+  });
+
+});
