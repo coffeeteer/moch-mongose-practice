88645d24c31e1166be793dad9a0e4e0a8bfbf04d
Coffeeteer <tgrieshiemer@gmail.com>
2017-09-17 18:56:13 -0400

    created some new Schema files including post.js, and comment.js

---
+++ src/comment.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    content: String,
+    user: {type: Schema.Types.ObjectId, ref: 'user'}
+    // Schema.Types.ObjectId assigns the path to ref
+    //the ref key refers to the model, in this case the User model
+});
